afd3dba3c3551d78b0e17ebbd597a2ef2ebf1b36
EaglePB2 <eagleforceleader@hotmail.com>
2022-11-16 12:47:08 +0800

    Update solution.c

--- Project Euler Archives/7. 10001st prime/C/solution.c
+++ Project Euler Archives/7. 10001st prime/C/solution.c
@@ -2,7 +2,7 @@
  
 int main()
 {
-    int numberToTest, divisor, numberOfPrime, counter;
+    int numberToTest, divisor, numberOfPrime;
     // when the number is N, so the divisor will start adding itself from 2 to N. If there's no any sufficient to make N % divisor = 0 (no remainder), then that must be a prime number; otherwise, the if(divisor == number) will turn false, so that number you have found is not a prime; then add the number to N+1, so on (until you had reached 100 based on your code below // 
         for(numberToTest = 1; numberToTest > 0; numberToTest++){
             for(divisor = 2; divisor < numberToTest; divisor++){
