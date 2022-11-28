9df4ab09988b393e267bb6e6f1d2a9e3312c9491
EaglePB2 <eagleforceleader@hotmail.com>
2022-11-28 17:31:16 +0800

    Create solution.cpp

---
+++ {PWB}/If-Else Programming/003. Determine Character Properties/C++/solution.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    char a;
+    cout << "Press Any Key : ";
+    cin >> a;
+
+    if (a >= 65 && a <= 90) {
+        cout << "Uppercase";
+    } else if (a >= 97 && a <= 122) {
+        cout << "Lowercase\n";
+    } else if (a >= 48 && a <= 57) {
+        cout << "Digit\n";
+    } else {
+        cout << "Symbol\n";
+    }
+}
