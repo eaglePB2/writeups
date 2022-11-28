9dde5185a771c8bd2ab466a9bace233f900693de
EaglePB2 <eagleforceleader@hotmail.com>
2022-11-28 15:49:20 +0800

    Create solution.cpp

---
+++ {PWB}/Simple Programming/001. Area and Circumference of a Circle/C++/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int rad;
+  float PI = 3.14, area, ci;
+  cout<<"Enter radius of circle: ";
+  cin>>rad;
+  area = PI * rad * rad;
+  cout << "Area of circle " << setprecision(2) << fixed << area << endl;
+  ci = 2 * PI * rad;
+  cout << "Circumference of circle " << setprecision(2) << fixed << ci << endl;
+  return 0;
+}
