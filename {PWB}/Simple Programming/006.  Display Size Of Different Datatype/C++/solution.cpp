c1ffda4e8d55a0ad3e7177b6ec92383dc4db434b
EaglePB2 <eagleforceleader@hotmail.com>
2022-11-28 17:01:06 +0800

    Create solution.cpp

---
+++ {PWB}/Simple Programming/006.  Display Size Of Different Datatype/C++/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+    cout<<"size of int \t\t"<<sizeof(int)<<endl;
+    cout<<"size of float \t\t"<<sizeof(float)<<endl;
+    cout<<"size of double \t\t"<<sizeof(double)<<endl;
+    cout<<"size of long \t\t"<<sizeof(long)<<endl;
+    cout<<"size of long double \t"<<sizeof(long double)<<endl;
+    cout<<"size of long long int \t"<<sizeof(long long int)<<endl;
+    cout<<"size of long long \t"<<sizeof(long long)<<endl;
+    return 0;
+}
